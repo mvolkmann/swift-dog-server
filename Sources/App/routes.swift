ecda5e09adf5ebde21c9511ca7005cdba6c573b0
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-13 14:53:05 -0600

    progress

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,6 +4,11 @@ import Vapor
 // in order to return from a route.
 struct Dog: Content {
     let id: Int
+    var name: String
+    var breed: String
+}
+
+struct NewDog: Content {
     let name: String
     let breed: String
 }
@@ -12,20 +17,22 @@ var dogMap: [Int: Dog] = [:]
 
 var lastId = 0
 
-func addDog(name: String, breed: String) {
+func addDog(name: String, breed: String) -> Dog {
     lastId += 1
-    dogMap[lastId] = Dog(id: lastId, name: name, breed: breed)
+    let dog = Dog(id: lastId, name: name, breed: breed)
+    dogMap[lastId] = dog
+    return dog
 }
 
-func initialize() {
-    addDog(name: "Maisey", breed: "Treeing Walker Coonhound")
-    addDog(name: "Ramsay", breed: "Native American Indian Dog")
-    addDog(name: "Oscar", breed: "German Shorthaired Pointer")
-    addDog(name: "Comet", breed: "Whippet")
+func setup() {
+    _ = addDog(name: "Maisey", breed: "Treeing Walker Coonhound")
+    _ = addDog(name: "Ramsay", breed: "Native American Indian Dog")
+    _ = addDog(name: "Oscar", breed: "German Shorthaired Pointer")
+    _ = addDog(name: "Comet", breed: "Whippet")
 }
 
 func routes(_ app: Application) throws {
-    initialize()
+    setup()
 
     app.get { _ in
         "It works!"
@@ -38,7 +45,7 @@ func routes(_ app: Application) throws {
 
     app.get("dog", ":id") { req -> Dog in
         guard let id = req.parameters.get("id", as: Int.self) else {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: "missing id param")
         }
 
         if let dog = dogMap[id] {
@@ -46,5 +53,55 @@ func routes(_ app: Application) throws {
         } else {
             throw Abort(.notFound)
         }
+    }
+
+    app.post("dog") { req -> Dog in
+        guard let byteBuffer = req.body.data else {
+            throw Abort(.badRequest, reason: "invalid or missing body")
+        }
+
+        do {
+            let newDog = try JSONDecoder().decode(NewDog.self, from: byteBuffer)
+            return addDog(name: newDog.name, breed: newDog.breed)
+        } catch {
+            throw Abort(.badRequest, reason: "failed to decode body to Dog")
+        }
+    }
+    
+    app.put("dog", ":id") { req -> Dog in
+        guard let id = req.parameters.get("id", as: Int.self) else {
+            throw Abort(.badRequest, reason: "missing id param")
+        }
+        
+        // This creates a copy of the struct.
+        guard var dog = dogMap[id] else {
+            throw Abort(.notFound, reason: "no dog with id \(id) found")
+        }
+        
+        guard let byteBuffer = req.body.data else {
+            throw Abort(.badRequest, reason: "invalid or missing body")
+        }
+
+        do {
+            let newDog = try JSONDecoder().decode(NewDog.self, from: byteBuffer)
+            // Update the copied struct.
+            dog.name = newDog.name
+            dog.breed = newDog.breed
+            
+            // Update the value in dogMap.
+            dogMap[id] = dog
+            return dog
+        } catch {
+            throw Abort(.badRequest, reason: "failed to decode body to Dog")
+        }
+    }
+
+    app.delete("dog", ":id") { req -> HTTPStatus in
+        guard let id = req.parameters.get("id", as: Int.self) else {
+            throw Abort(.badRequest, reason: "missing id param")
+        }
+        
+        let dog = dogMap.removeValue(forKey: id)
+        return dog == nil ? .notFound : .ok
     }
 }
